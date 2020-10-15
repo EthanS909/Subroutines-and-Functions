91c120dbd65aeafd7133dacd6b2f440d16f4b07e
TheEthBeast <72858151+TheEthBeast@users.noreply.github.com>
2020-10-15 10:59:01 +0100

    task 2

---
+++ Subroutines and Functions/Subroutines and Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Subroutines_and_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 34;
+            int c = 75;
+
+            task2(a, b, c);
+        }
+
+        static void task2(int a, int b, int c)
+        {
+            int result = a + b + c;
+            Console.WriteLine(result);
+        }
+    }
+}
