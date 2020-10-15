97efe2253121099515b66250dabdccd5d804755c
TheEthBeast <72858151+TheEthBeast@users.noreply.github.com>
2020-10-15 11:04:21 +0100

    task 3

--- Subroutines and Functions/Subroutines and Functions/Program.cs
+++ Subroutines and Functions/Subroutines and Functions/Program.cs
@@ -9,14 +9,27 @@ namespace Subroutines_and_Functions
             int a = 23;
             int b = 34;
             int c = 75;
-
             task2(a, b, c);
+
+            int d = 36;
+            int e = 78;
+            int f = 58;
+            int g = 0;
+            g = task3(d, e, f);
+            Console.WriteLine(g);
         }
 
         static void task2(int a, int b, int c)
         {
             int result = a + b + c;
             Console.WriteLine(result);
+        }
+
+        static int task3(int a, int b, int c)
+        {
+            int result = a + b + c;
+
+            return result;
         }
     }
 }
